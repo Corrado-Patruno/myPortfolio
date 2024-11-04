ec0515b56f94d0b60c6e24126de3be0a46d8faba
Corrado-Patruno <111148791+Corrado-Patruno@users.noreply.github.com>
2024-11-04 19:51:23 +0100

    Commit 6

--- assets/js/sound.js
+++ assets/js/sound.js
@@ -1,14 +1,14 @@
-function playSound() {
-  const audio = new Audio('./assets/sound_effects/soundbutton.wav');
-  audio.volume = 0.5;
-  audio.play();
-}
+ // function playSound() {
+  //  const audio = new Audio('./assets/sound_effects/soundbutton.wav');
+  //  audio.volume = 0.5;
+  //  audio.play();
+ // }
 
 
-function playSound2() {
-  const audio = new Audio('./assets/sound_effects/switch.wav');
-  audio.volume = 0.5;
-  audio.play();
-}
+ // function playSound2() {
+  //  const audio = new Audio('./assets/sound_effects/switch.wav');
+   // audio.volume = 0.5;
+   // audio.play();
+ // }
 
 
